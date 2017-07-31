bea0545f4807065e73510f59838d9c6c04f11400
Rogerio Angeliski <rogerio.angeliski@bluesoft.com.br>
2017-07-31 08:31:35 -0300

    Adicionando arquivos e comandos

--- git/init.sh
+++ git/init.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 git config --global user.name "Rogerio Angeliski"
 git config --global user.email rogerio.angeliski@bluesoft.com.br
---
+++ new_installation/install_tomcat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl http://mirror.sdunix.com/apache/tomcat/tomcat-8/v8.0.23/bin/apache-tomcat-8.0.23.tar.gz
