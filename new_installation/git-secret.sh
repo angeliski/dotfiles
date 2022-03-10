cec83e22f357cbcd6826f822aff46f99b561f405
Rogerio Angeliski <angeliski@hotmail.com>
2022-03-10 20:34:23 -0300

    add git-secret

---
+++ new_installation/git-secret.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+install_git_secret () {
+    sudo sh -c "echo 'deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' >> /etc/apt/sources.list"
+    wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
+    sudo apt-get update && sudo apt-get install -y git-secret	
+}
+
+git secret --version || install_git_secret
