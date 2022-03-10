e7e5270b7fbc95aa866b8ea5c525191b79dccde7
Rogerio Angeliski <angeliski@hotmail.com>
2022-03-10 19:25:46 -0300

    change font install

--- new_installation/agnoster-font-install.sh
+++ new_installation/agnoster-font-install.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 if [ ! -e ~/.fonts_installed ]; then
-	mkdir -p ~/.fonts_theme
-
-	echo 'Iniciando download do repositório'
-	git clone https://github.com/powerline/fonts.git --depth=1 ~/.fonts_theme
-
-	echo 'Instalando fontes'
-	~/.fonts_theme/install.sh
-	rm -rf ~/.fonts_theme
-	mkdir -p ~/.fonts_installed
-
+	sudo apt-get install fonts-powerline && mkdir -p ~/.fonts_installed
 	echo 'Fim da instalação'
-       
 else
     echo 'Fontes instaladas'
 fi
