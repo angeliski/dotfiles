2c55d8a8b3c44e59975a64ac2060c3c2409e189f
Rogerio Angeliski <angeliski@hotmail.com>
2022-03-10 19:04:19 -0300

    change asdf install

--- new_installation/asdf-manager.sh
+++ new_installation/asdf-manager.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
-# install dependencies https://asdf-vm.com/guide/getting-started.html#_1-install-dependencies
-sudo apt install curl git -y
-
 if [ ! -e ~/.asdf/asdf.sh ]
 then
- git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+   # install dependencies https://asdf-vm.com/guide/getting-started.html#_1-install-dependencies
+   sudo apt install curl git -y
+   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
 fi
 
-. $HOME/.asdf/asdf.sh
+. ~/.asdf/asdf.sh
 
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-diff --git a/new_installation/zsh.sh b/new_installation/zsh.sh
+asdf info | grep ruby  || asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf info | grep golang  || asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
