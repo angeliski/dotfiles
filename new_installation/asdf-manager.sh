24a71e45b45cdb1cabc5c9ff0b775d3e1f6eac08
Rogerio Angeliski <angeliski@hotmail.com>
2022-03-10 18:15:48 -0300

    update asdf install

--- new_installation/asdf-manager.sh
+++ new_installation/asdf-manager.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # install dependencies https://asdf-vm.com/guide/getting-started.html#_1-install-dependencies
-apt install curl git -y
+sudo apt install curl git -y
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
 
+. $HOME/.asdf/asdf.sh
 
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
